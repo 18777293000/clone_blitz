4b3192bf77d5a34faa1622172701151e5d101353
杨铭 <webming@bitkan.com>
2021-09-22 17:47:58 +0800

    unableto detect target browser

--- scripts/start.js
+++ scripts/start.js
@@ -1,20 +1,186 @@
-"use strict";
+// "use strict";
 
-console.log("running");
-//resource allocation enviorment variables, to decide proceed direction
-process.env.BABEL_ENV = "development";
-process.env.NODE_ENV = "development";
+// console.log("running");
+// //resource allocation enviorment variables, to decide proceed direction
+// process.env.BABEL_ENV = "development";
+// process.env.NODE_ENV = "development";
 
-process.on("unhandledRejection", err => {
+// process.on("unhandledRejection", err => {
+//   throw err;
+// });
+
+// require("../config/env");
+
+// const fs = require("fs");
+// const chalk = require('react-dev-utils/chalk');
+// const webpack = require('webpack');
+// const WebpackDevServe = require('webpack-dev-server');
+// const clearConsole = require('react-dev-utils/clearConsole');
+// const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
+// const {
+//   choosePort,
+//   createCompiler,
+//   prepareProxy,
+//   prepareUrls,
+// } = require('react-dev-utils/WebpackDevServerUtils');
+// const openBrowser = require('react-dev-utils/openBrowser');
+// const paths = require('../config/paths');
+// const configFactory = require('../config/webpack.config');
+// const createDevServerConfig = require('../config/webpackDevServer.config');
+
+// const useYarn = fs.existsSync(paths.yarnLockFile);
+// const isInteractive = process.stdout.isTTY;
+// console.log('running');
+// //在这里检查src文件下的index.js和public文件下的index.html
+// if(!checkRequiredFiles([paths.appHtml, paths.appIndexJs])){
+//   console.log("existsing");
+//   process.exit(1);
+// }
+// const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 3000;
+// console.log("DEFAULT_PORT", DEFAULT_PORT);
+// const HOST = process.env.HOST || '0.0.0.0';
+// if(process.env.HOST){
+//   console.log(
+//     chalk.cyan(
+//       `Attempting to bind to HOST environment variable: ${chalk.yellow(
+//         chalk.bold(HOST)
+//       )}`
+//     )
+//   );
+//   console.log(
+//     `If this was unintentional, check that you haven't mistakenly set it in your shell.`
+//   );
+//   console.log(
+//     `Learn more here: ${chalk.yellow('https://bit.ly/CRA-advanced-config')}`
+//   );
+//   console.log();
+// }
+
+// console.log('running2');
+
+// const { checkBrowsers } = require('react-dev-utils/browsersHelper');
+
+// console.log('running3', paths.appPath, isInteractive);
+
+// checkBrowsers(paths.appPath, isInteractive)
+//   .then(() => {
+
+//     console.log('running4', 3000);
+
+//     return choosePort(HOST, DEFAULT_PORT);
+//   })
+//   .then(port => {
+
+//     console.log('running5', port);
+
+//     if(port == null){
+//       return
+//     }
+
+//     const config = configFactory('development');
+//     const protocol = process.env.HTTPS === 'true' ? 'https' : 'http';
+//     const appName = require(paths.appPackageJson).name;
+//     const useTypeScript = fs.existsSync(paths.appTsConfig);
+//     const tscCompileOnError = process.env.TSC_COMPILE_ON_ERROR === 'true';
+//     const urls = prepareUrls(
+//       protocol,
+//       HOST,
+//       port,
+//       paths.publicUrlOrPath.slice(0, -1)
+//     );
+//     const devSocket = {
+//       warnings: warnings =>
+//         devServer.sockWrite(devServer.sockets, 'warnings', warnings),
+//       errors: errors => 
+//         devServer.sockWrite(devServer.sockets, 'errors', errors),
+//     };
+//     const compiler = createCompiler({
+//       appName,
+//       config,
+//       devSocket,
+//       urls,
+//       useYarn,
+//       useTypeScript,
+//       tscCompileOnError,
+//       webpack
+//     });
+//     const proxySetting = require(paths.appPackageJson).proxy;
+//     const proxyConfig = prepareProxy(
+//       proxySetting,
+//       paths.appPublic,
+//       paths.publicUrlOrPath,
+//     );
+
+//     const serverConfig = createDevServerConfig(
+//       proxyConfig,
+//       urls.lanUrlForConfig
+//     );
+
+//     const devServer = new WebpackDevServe(compiler, serverConfig);
+//     devServer.listen(port, HOST, err => {
+//       if(err){
+//         return console.log(12, err);
+//       }
+//       if(isInteractive){
+//         clearConsole();
+//       }
+
+//       if(process.env.NODE_PATH){
+//         console.log(
+//           chalk.yellow(
+//             'Setting NODE_PATH to resolve modules absolutely has been deprecated in favor of setting baseUrl in jsconfig.json (or tsconfig.json if you are using TypeScript) and will be removed in a future major release of create-react-app.'
+//           )
+//         );
+//         console.log();
+//       }
+//       console.log(chalk.cyan('starting tthe development server... \n'));
+//       openBrowser(urls.localUrlForBrowser);
+//     });
+
+//     ['SIGINT', 'SIGTERM'].forEach(function(sig) {
+//       process.on(sig, function(){
+//         devServer.close();
+//         process.exit(1);
+//       });
+//     });
+
+//     if(isInteractive || process.env.CI !== 'true'){
+//       process.stdin.on('end', function() {
+//         devServer.close();
+//         process.exit();
+//       });
+//       process.stdin.resume();
+//     }
+//   })
+//   .cath(err => {
+//     console.log('error213', err);
+//     if(err && err.message){
+//       console.log(err.message);
+//     }
+//     process.exit();
+//   });
+
+'use strict';
+
+// Do this as the first thing so that any code reading it knows the right env.
+process.env.BABEL_ENV = 'development';
+process.env.NODE_ENV = 'development';
+
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on('unhandledRejection', err => {
   throw err;
 });
 
-require("../config/env");
+// Ensure environment variables are read.
+require('../config/env');
 
-const fs = require("fs");
+
+const fs = require('fs');
 const chalk = require('react-dev-utils/chalk');
 const webpack = require('webpack');
-const WebpackDevServe = require('webpack-dev-server');
+const WebpackDevServer = require('webpack-dev-server');
 const clearConsole = require('react-dev-utils/clearConsole');
 const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
 const {
@@ -30,20 +196,21 @@ const createDevServerConfig = require('../config/webpackDevServer.config');
 
 const useYarn = fs.existsSync(paths.yarnLockFile);
 const isInteractive = process.stdout.isTTY;
-console.log('running');
-//在这里检查src文件下的index.js和public文件下的index.html
-if(!checkRequiredFiles([paths.appHtml, paths.appIndexJs])){
-  console.log("existsing");
+// Warn and crash if required files are missing
+if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
   process.exit(1);
 }
+
+// Tools like Cloud9 rely on this.
 const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 3000;
-console.log("DEFAULT_PORT", DEFAULT_PORT);
 const HOST = process.env.HOST || '0.0.0.0';
-if(process.env.HOST){
+console.log(DEFAULT_PORT, HOST);
+
+if (process.env.HOST) {
   console.log(
     chalk.cyan(
       `Attempting to bind to HOST environment variable: ${chalk.yellow(
-        chalk.bold(HOST)
+        chalk.bold(process.env.HOST)
       )}`
     )
   );
@@ -56,25 +223,19 @@ if(process.env.HOST){
   console.log();
 }
 
-console.log('running2');
-
+// We require that you explicitly set browsers and do not fall back to
+// browserslist defaults.
 const { checkBrowsers } = require('react-dev-utils/browsersHelper');
-
-console.log('running3', paths.appPath, isInteractive);
-
 checkBrowsers(paths.appPath, isInteractive)
   .then(() => {
-
-    console.log('running4', 3000);
-
+    // We attempt to use the default port but if it is busy, we offer the user to
+    // run on a different port. `choosePort()` Promise resolves to the next free port.
     return choosePort(HOST, DEFAULT_PORT);
   })
   .then(port => {
-
-    console.log('running5', port);
-
-    if(port == null){
-      return
+    if (port == null) {
+      // We have not found a port.
+      return;
     }
 
     const config = configFactory('development');
@@ -91,9 +252,10 @@ checkBrowsers(paths.appPath, isInteractive)
     const devSocket = {
       warnings: warnings =>
         devServer.sockWrite(devServer.sockets, 'warnings', warnings),
-      errors: errors => 
+      errors: errors =>
         devServer.sockWrite(devServer.sockets, 'errors', errors),
     };
+    // Create a webpack compiler that is configured with custom messages.
     const compiler = createCompiler({
       appName,
       config,
@@ -102,30 +264,34 @@ checkBrowsers(paths.appPath, isInteractive)
       useYarn,
       useTypeScript,
       tscCompileOnError,
-      webpack
+      webpack,
     });
+    // Load proxy config
     const proxySetting = require(paths.appPackageJson).proxy;
     const proxyConfig = prepareProxy(
       proxySetting,
       paths.appPublic,
-      paths.publicUrlOrPath,
+      paths.publicUrlOrPath
     );
-
+    // Serve webpack assets generated by the compiler over a web server.
     const serverConfig = createDevServerConfig(
       proxyConfig,
       urls.lanUrlForConfig
     );
-
-    const devServer = new WebpackDevServe(compiler, serverConfig);
+    const devServer = new WebpackDevServer(compiler, serverConfig);
+    // Launch WebpackDevServer.
     devServer.listen(port, HOST, err => {
-      if(err){
-        return console.log(12, err);
+      if (err) {
+        return console.log(err);
       }
-      if(isInteractive){
+      if (isInteractive) {
         clearConsole();
       }
 
-      if(process.env.NODE_PATH){
+      // We used to support resolving modules according to `NODE_PATH`.
+      // This now has been deprecated in favor of jsconfig/tsconfig.json
+      // This lets you use absolute paths in imports inside large monorepos:
+      if (process.env.NODE_PATH) {
         console.log(
           chalk.yellow(
             'Setting NODE_PATH to resolve modules absolutely has been deprecated in favor of setting baseUrl in jsconfig.json (or tsconfig.json if you are using TypeScript) and will be removed in a future major release of create-react-app.'
@@ -133,18 +299,20 @@ checkBrowsers(paths.appPath, isInteractive)
         );
         console.log();
       }
-      console.log(chalk.cyan('starting tthe development server... \n'));
+
+      console.log(chalk.cyan('Starting the development server...\n'));
       openBrowser(urls.localUrlForBrowser);
     });
 
     ['SIGINT', 'SIGTERM'].forEach(function(sig) {
-      process.on(sig, function(){
+      process.on(sig, function() {
         devServer.close();
-        process.exit(1);
+        process.exit();
       });
     });
 
-    if(isInteractive || process.env.CI !== 'true'){
+    if (isInteractive || process.env.CI !== 'true') {
+      // Gracefully exit when stdin ends
       process.stdin.on('end', function() {
         devServer.close();
         process.exit();
@@ -152,10 +320,9 @@ checkBrowsers(paths.appPath, isInteractive)
       process.stdin.resume();
     }
   })
-  .cath(err => {
-    console.log('error213', err);
-    if(err && err.message){
+  .catch(err => {
+    if (err && err.message) {
       console.log(err.message);
     }
-    process.exit();
+    process.exit(1);
   });

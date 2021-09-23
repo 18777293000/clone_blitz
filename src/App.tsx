9dfff023a019635eaa05d7916c0661a2483c0174
杨铭 <webming@bitkan.com>
2021-09-23 16:30:13 +0800

    display content success

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useObservable } from 'rxjs-hooks';
 import { filter } from 'rxjs/operators';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+const Notice = lazy(() => import('./render/help/pages/notices/notices'));
 const App = () => {
 
   return (
---
+++ src/types.d.ts
@@ -0,0 +1,41 @@
+
+// 加密, 解密
+declare module 'jsencrypt';
+
+// socket 压缩、解压库
+declare module 'pako/dist/pako_inflate';
+
+// lodash
+declare module 'lodash.kebabcase';
+
+// d3
+declare module 'd3';
+
+declare module 'papaparse';
+
+declare module 'qiniu-js';
+
+declare module 'react-froala-wysiwyg';
+declare module 'react-froala-wysiwyg/FroalaEditorView';
+declare module 'simplemde';
+declare module 'jquery';
+declare var $;
+declare var gapi;
+
+declare module 'lodash-es/clamp';
+
+declare module 'lodash-move';
+
+declare module 'react-window';
+
+declare module 'react-virtualized-auto-sizer';
+
+interface Window {
+  TradingView: any;
+  dataLayer: any;
+  gtag: any;
+  gtm: any;
+  ga: any;
+  HTMLElement: any;
+  safari: any;
+}
